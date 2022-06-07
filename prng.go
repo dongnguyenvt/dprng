82d022c36f49fde505e9cc04f3bce123009280e6
Dong Nguyen <1096.dev.ext@virz.dev>
2022-06-07 12:51:18 +0700

    implement deterministic rand reader

---
+++ prng.go
@@ -0,0 +1,26 @@
+package dprng
+
+import "io"
+
+func NewRandReader(seed int) io.Reader {
+	if seed < 0 {
+		seed = -seed
+	}
+	return &prngReader{s: seed % seedSize}
+}
+
+// prngReader implements an io.Reader that returns pseudorandom bytes.
+// ensure every call with will return same random pattern
+type prngReader struct{ s int }
+
+// Read implements io.Reader.Read.
+func (r *prngReader) Read(p []byte) (int, error) {
+	idx := r.s
+	var copied int
+	for copied < len(p) {
+		copied += copy(p[copied:], seedData[idx:])
+		idx += copied
+		idx = idx % seedSize
+	}
+	return copied, nil
+}
---
+++ prng_test.go
@@ -0,0 +1,25 @@
+package dprng
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRandReader(t *testing.T) {
+	testRandReader(t, 0)
+	testRandReader(t, 100)
+	testRandReader(t, 5000)
+}
+
+func testRandReader(t *testing.T, seed int) {
+	r := NewRandReader(seed)
+	buf := make([]byte, 128)
+	if _, err := r.Read(buf); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(fmt.Sprintf("0x%x", buf))
+	if _, err := r.Read(buf); err != nil {
+		t.Fatal(err)
+	}
+	t.Log(fmt.Sprintf("0x%x", buf))
+}
