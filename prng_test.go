44a61b9938cf7fa3d5af91ae39810f2c1a4b1371
Dong Nguyen <1096.dev.ext@virz.dev>
2022-06-07 18:15:51 +0700

    update tests

--- prng_test.go
+++ prng_test.go
@@ -6,20 +6,29 @@ import (
 )
 
 func TestRandReader(t *testing.T) {
-	testRandReader(t, 0)
-	testRandReader(t, 100)
-	testRandReader(t, 5000)
+	testRandReader(t, 0, "0x86e02a0174bf3ae5cb5683b220f6756e")
+	testRandReader(t, 0, "0x86e02a0174bf3ae5cb5683b220f6756e")
+	testRandReader(t, 100, "0xd1f14a9b5b9e2b1240a2231ad7987c7b")
+	testRandReader(t, 100, "0xd1f14a9b5b9e2b1240a2231ad7987c7b")
+	testRandReader(t, 5000, "0xaaec29002fc64b7ebaa37428ca828b79")
+	testRandReader(t, 5000, "0xaaec29002fc64b7ebaa37428ca828b79")
 }
 
-func testRandReader(t *testing.T, seed int64) {
+func testRandReader(t *testing.T, seed int64, expected string) {
 	r := NewRandReader(seed)
-	buf := make([]byte, 128)
+	buf := make([]byte, 16)
 	if _, err := r.Read(buf); err != nil {
 		t.Fatal(err)
 	}
-	t.Log(fmt.Sprintf("0x%x", buf))
+	s := fmt.Sprintf("0x%x", buf)
+	if expected != s {
+		t.Fatal(s)
+	}
 	if _, err := r.Read(buf); err != nil {
 		t.Fatal(err)
 	}
-	t.Log(fmt.Sprintf("0x%x", buf))
+	s = fmt.Sprintf("0x%x", buf)
+	if expected != s {
+		t.Fatal(s)
+	}
 }
